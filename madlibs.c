3abccc65200701792e84993d2e5ed9ddd347834e
Lauren Hockaday <lauren.hockaday21@gmail.com>
2024-12-05 00:30:15 +0000

    Group Final

--- madlibs.c
+++ madlibs.c
@@ -1,12 +1,12 @@
-//Team Members:Lauren Hockaday, Rashin Raisa, Adrian Ochaa
+//Team Members:Lauren Hockaday, Rashin Raisa, Adrian Ochoa
 //Purpose: Final Group Project
 
 #include <stdio.h>
 void();
 void();
-void();
-void();
-void();
+void openFile();//Adrian
+void scanLetter();//Lauren
+void displayFile();//Rashin
 #define WORDS 8
 #define WORDLENGTH 100
 int main(){
