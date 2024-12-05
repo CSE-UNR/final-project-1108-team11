8a0e71331e5f224517946582473681049de7f95e
Lauren Hockaday <lauren.hockaday21@gmail.com>
2024-12-05 00:14:56 +0000

    Group Final

--- madlibs.c
+++ madlibs.c
@@ -10,11 +10,23 @@ void();
 #define WORDS 8
 #define WORDLENGTH 100
 int main(){
-
-printf
-
-
-
+	char inwords [WORDS][WORDSLENGTH];
+	printf("Please enter an adjective: ");
+		scanf("%s", inwords);
+	printf("Please enter a noun: ");
+		scanf("%s", inwords);
+	printf("Please enter an adjective: ");
+		scanf("%s", inwords);
+	printf("Please enter a verb: ");
+		scanf("%s", inwords);
+	printf("Please enter a verb: ");
+		scanf("%s", inwords);
+	printf("Please enter an adjective: ");
+		scanf("%s", inwords);
+	printf("Please enter a noun: ");
+		scanf("%s", inwords);
+	printf("Please enter an adjective: ");
+		scanf("%s", inwords);
 
 
 return 0;
