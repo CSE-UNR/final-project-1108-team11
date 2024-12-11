22d61495add603adf0f7243ff79692b9d5fe6d2d
Lauren Hockaday <lauren.hockaday21@gmail.com>
2024-12-11 05:55:19 +0000

    final

--- madlibs.c
+++ madlibs.c
@@ -2,52 +2,54 @@
 //Purpose: Final Group Project
 
 #include <stdio.h>
-void getInput(int numwords, int stringlength, char inwords[][stringlength]);
-void storeWords(int stringlength, int linelength, int numlines, char inwords[][stringlength]);
+void getInput(int numwords, int stringlength, int filelength, char inwords1[], char inwords2[][stringlength]);
+void storeWords(int stringlength, int linelength, int numlines, char inwords2[][stringlength]);
 void storeFile(FILE* fp, int numlines, int linelength, char filearray[][linelength]);
-void scanLetter(int numwords, int stringlength, char filearray[][stringlength], char inwords[][stringlength]);
-void displayFile(int filelines, int NUM_WORDS, int linesize1, int linesize2, char filearray[][linesize1], char inwords[][linesize2]);
+void scanLetter(int numwords, int stringlength, char filearray[][stringlength], char inwords2[][stringlength]);
+void displayFile(int NUM_WORDS, int numLines, int linesize1, int linesize2, char filearray[][linesize1], char inwords2[][linesize2]);
 #define WORDS 8
 #define WORDLENGTH 100
 #define FILEWIDTH 100
-#define FILEHEIGHT 100
+#define FILEHEIGHT 17
 #define FILENAME "madlib2.txt"
 int main(){
-char inwords[WORDS][WORDLENGTH];
+char inWords2 [WORDS][WORDLENGTH];
 char fileArray[FILEHEIGHT][FILEWIDTH];
-char line_end= '\0';
+char inWords1 [WORDS];
 
 	FILE* fp;
 	fp=fopen(FILENAME,"r");
 		if(fp==NULL){
 			printf("Can't open file.\n");
-			return 1;
+			return 0;
 		}
 	
 	storeFile(fp, FILEWIDTH, FILEHEIGHT, fileArray);
-	getInput(WORDS, WORDLENGTH, inwords);
+	scanLetter(FILEWIDTH, FILEHEIGHT, fileArray, inWords2);
 	
-	//scanLetter(FILEWIDTH, FILEHEIGHT, fileArray, inwords);
-	//storeWords(WORDS, WORDLENGTH, line_end, inwords);
+	getInput(WORDS, WORDLENGTH, FILEWIDTH, inWords1, inWords2);
+	storeWords(WORDS, WORDLENGTH, FILEHEIGHT, inWords2);
 	
-	//displayFile(WORDS, WORDLENGTH, FILEWIDTH, FILEHEIGHT, fileArray,inwords);
+	displayFile(WORDS, WORDLENGTH, FILEWIDTH, FILEHEIGHT, fileArray,inWords2);
 	
 	fclose(fp);
 return 0;
 }
-void getInput(int numwords, int stringlength, char inwords[][stringlength]){
+void getInput(int numwords, int stringlength, int filelength, char inwords1[], char inwords2[][stringlength]){
 	for(int i=0; i<numwords; i++){
-	switch(i){
+		for(int j=0; j<stringlength; j++){
+	switch(inwords1[i]){
 		case 'N': printf("Please enter a noun: ");
-		scanf("%s", inwords[i]);
+		scanf("%s", inwords2 [i][j]);
 			break;
 		case 'V':printf("Please enter a verb: ");
-		scanf("%s", inwords[i]);
+		scanf("%s", inwords2 [i][j]);
 			break;
 		case 'A':printf("Please enter an adjective: ");
-		scanf("%s", inwords[i]);
+		scanf("%s", inwords2 [i][j]);
 			break;
 			}
+		}
 	}
 }
 
@@ -58,40 +60,41 @@ void storeFile(FILE* fp, int numlines, int linelength, char filearray[][lineleng
 		}
 	}
 }
-void storeWords(int stringlength, int linelength, int numlines, char inwords[][stringlength]){
+void storeWords(int stringlength, int linelength, int numlines, char inwords2[][stringlength]){
 	for(int i=0; i<linelength; i++){
 		for(int j=0; j<numlines; j++){
-		scanf("%s", inwords[j][i]);
+		scanf("%s", inwords2[i][j]);
 		}
 	}
 }
-void displayFile(int filelines, int NUM_WORDS, int linesize1, int linesize2, char filearray[][linesize1], char inwords[][linesize2]){
-	for(int i=0; i<filelines; i++){
+void displayFile(int NUM_WORDS, int numLines, int linesize1, int linesize2, char filearray[][linesize1], char inwords2[][linesize2]){
+	for(int i=0; i<NUM_WORDS; i++){
 		if(filearray[i][0]=='A'||filearray[i][0]=='V'||filearray[i][0]=='N'){
-			for(int j=0; j<NUM_WORDS; j++){
-				printf("%s\n", inwords[i][j]);
+			for(int j=0; j<numLines; j++){
+				printf("%s\n", inwords2[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	for(int i=0; i<filelines; i++){
-	switch(i){
+	for(int i=0; i<numLines; i++){
+	switch(filearray[i][0]){
 		case 'N':
-		printf("%s\n", inwords);
+		printf("%s\n", inwords2[i]);
 		break;
 		case 'V':
-		printf("%s\n", inwords);
+		printf("%s\n", inwords2[i]);
 		break;
 		case 'A':
-		printf("%s\n", inwords);
+		printf("%s\n", inwords2[i]);
 		break;
 		}
 	}
 }
-void scanLetter(int numwords, int stringlength, char filearray[][stringlength], char inwords[][stringlength]){
+void scanLetter(int numwords, int stringlength, char filearray[][stringlength], char inwords2[][stringlength]){
+	int j;
 	for(int i=0; i< numwords; i++){
 		if(filearray[i][0]=='A'||filearray[i][0]=='N'||filearray[i][0]=='V'){
-			printf("%s\n", inwords[i]);
+			printf("%s\n", inwords2[i][j]);
 		}
 	}
 }
